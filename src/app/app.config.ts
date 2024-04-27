6d9b7155e7f996484f7da32c12dfd2e18af96eeb
aman.b.yadav <aman.b.yadav@accenture.com>
2024-04-27 15:44:56 +0530

    firebase integrated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { AngularFireModule } from '@angular/fire/compat';
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom( AngularFireModule.initializeApp(environment.firebase)) ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production:true,
+    firebase : {
+        apiKey: "AIzaSyBoCe8SJmmIPO6PgNzeixowduJ_0btfo7c",
+        authDomain: "ay-todo-app.firebaseapp.com",
+        projectId: "ay-todo-app",
+        storageBucket: "ay-todo-app.appspot.com",
+        messagingSenderId: "137865505489",
+        appId: "1:137865505489:web:4f396cade25f5c07a3b3f0",
+        measurementId: "G-RHVFHXDSGW"
+      }    
+};
